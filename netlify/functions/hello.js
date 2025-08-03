deb41ebacc59a9c2b1a391e144c8b8143887758d
saellis <stephen.anthony.ellis@gmail.com>
2025-08-03 13:54:55 -0400

    hello name

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,7 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
+  const name = event.queryStringParameters.name || "World";
+  return {
     statusCode: 200,
-    body: "Hello, World",
-  });
+    body: `Hello, ${name}`,
+  };
 };
